72240f4844c63859797ecd97027249170fd772be
Sinan YALÇINKAYA <sinanyalcinkaya@gmail.com>
2020-06-15 23:18:38 +0300

    auth eklendi

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ router.post("/logout", (req, res) => {
   res.json({ ok: true });
 });
 
-// Export the server middleware
+// Export the server middleware.
 export default {
   path: "/api",
   handler: router
